666843ab54dbf5e41f81526216af4bd88f702030
orisme12 <carlosbonet33@gmail.com>
2024-02-06 23:01:29 -0500

    Add new colors to materialKit

--- packages/kit/types/colors.d.ts
+++ packages/kit/types/colors.d.ts
@@ -1,14 +1,47 @@
-export const colors: {
-  white: string
-  black: string
-  red: {
+export namespace colors {
+  let white: string
+  let black: string
+  let red: {
     50: string
     100: string
+    200: string
+    300: string
+    400: string
+    500: string
+    600: string
+    700: string
+    800: string
+    900: string
+    950: string
   }
-  'blue-gray': {}
-  purple: {}
-  pink: {}
-  neutral: {}
-  gray: {}
+  let slate: {
+    50: string
+    100: string
+    200: string
+    300: string
+    400: string
+    500: string
+    600: string
+    700: string
+    800: string
+    900: string
+    950: string
+  }
+  let purple: {
+    50: string
+    100: string
+    200: string
+    300: string
+    400: string
+    500: string
+    600: string
+    700: string
+    800: string
+    900: string
+    950: string
+  }
+  let pink: {}
+  let neutral: {}
+  let gray: {}
 }
 //# sourceMappingURL=colors.d.ts.map
