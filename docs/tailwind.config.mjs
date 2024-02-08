fd92b6a385665dc7d388f6af66d2a79d8535f31d
castrogarciajs <johancs.mm@gmail.com>
2024-02-07 22:41:05 -0500

    Add docs with astro

--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import withKIT from '@kit-ui/material'
+
+export default withKIT({
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+})
