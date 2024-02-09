120f36ce01f1889fb9565aa1cbe55017cd0a6ef3
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-02-09 09:39:45 -0500

    Fix formatter code

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -3,7 +3,7 @@ import withNextra from 'nextra'
 const nextConfig = withNextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
 })
 
 export default nextConfig
